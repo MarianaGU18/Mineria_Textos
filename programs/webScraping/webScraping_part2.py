f316e3fa16b5cf25ee8e1e1a696024e902a97dc7
Mariana Gomez <marianagomezurbano@gmail.com>
2025-04-28 20:50:33 -0600

    Add error logging and enhance web scraping for EL PAIS articles
    - Created a new CSV file for logging errors encountered during scraping.
    - Updated the web scraping script to extract additional information: 'etiqueta' and 'localizacion'.
    - Improved content extraction logic to handle cases where expected elements are not found.
    - Adjusted output paths to reflect new directory structure for processed articles and error logs.

--- programs/webScraping/webScraping_part2.py
+++ programs/webScraping/webScraping_part2.py
@@ -21,16 +21,19 @@ with open('log_ejecuciones/ELPAIS_ESP/urlsExtraidas.csv', 'r', encoding='utf-8')
     for row in reader:
         urlsArticulos[row[0]] = row[1]
 
+#urlsArticulos = dict(list(urlsArticulos.items())[:10])  # Limitar a los primeros 100 artículos
+
+
 # Configurar Selenium
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-extensions')
 options.add_argument('--blink-settings=imagesEnabled=false')
-s = Service(os.path.dirname(os.path.abspath(__file__)) + '/chromedriver.exe')
+chromedriver_path = Service(os.path.dirname(os.path.abspath(__file__)) + '/chromedriver.exe')
 
 #s = Service('C:/Servicio Social/Mineria_Textos/chromedriver.exe')
 
 
-driver = webdriver.Chrome(options=options, service=s)
+driver = webdriver.Chrome(options=options, service=chromedriver_path)
 
 #for articulo, snap in urlsArticulos.items():
 for articulo, snap in tqdm(urlsArticulos.items(), desc="Procesando artículos"):
@@ -44,7 +47,7 @@ for articulo, snap in tqdm(urlsArticulos.items(), desc="Procesando artículos"):
         articuloError.append(linkArticulo)
         continue
 
-    titulo, subtitulo, articuloContenido = "NONE", "NONE", "NONE"
+    titulo, subtitulo, localizacion, articuloContenido, etiqueta = ["NONE"] * 5
     articuloFinal = []
 
     try:
@@ -61,14 +64,58 @@ for articulo, snap in tqdm(urlsArticulos.items(), desc="Procesando artículos"):
     except NoSuchElementException:
         pass
 
-    contenido = driver.find_elements(By.CLASS_NAME, "articulo-contenido")
-    if contenido:
-        contenido = contenido[0].find_elements(By.CLASS_NAME, "contenido")
-    else:
-        contenido = driver.find_elements(By.CLASS_NAME, "paragraph")
+    try:
+        etiqueta = driver.find_elements(By.CSS_SELECTOR, ".cs_t_l, ._db, .a_k, ._df, .k, .kicker, .uppercase")
 
+        if etiqueta:  # Verifica si se encontró al menos un elemento
+            etiqueta = etiqueta[0].text  # Obtiene el texto del primer elemento
+        else:
+            etiqueta = "No encontrada etiqueta"
+    except NoSuchElementException:
+        pass
+
+
+    # Extraer la etiqueta, puede ser una categoría o algo similar (Cambio realizado aquí)
+    try:
+        localizacion = driver.find_elements(By.CSS_SELECTOR, ".articulo-localizacion,.capitalize,.color_black")
+        
+        if localizacion:  # Verifica si se encontró al menos un elemento
+            localizacion = localizacion[0].text  # Obtiene el texto del primer elemento
+        else:
+            localizacion = "No encontrada ubicación"
+    except NoSuchElementException:
+        pass
+   
+    
+    try:
+        articuloContenido = driver.find_elements(By.CSS_SELECTOR, '.articulo-cuerpo, .a_b, .article_body, .color_gray_dark, .a_c, .clearfix, .c_d, p[class=""]')
+        
+        if articuloContenido:  # Si encontró al menos uno con las clases
+            articuloContenido = articuloContenido[0].text
+        else:
+            # Si NO encontró nada, buscar en todos los <p>
+            parrafos = driver.find_elements(By.TAG_NAME, "p")
+            if parrafos:
+                articuloContenido = " ".join(p.text for p in parrafos if p.text.strip())
+            else:
+                articuloContenido = "No encontrada contenido"
+    except NoSuchElementException:
+        articuloContenido = "No encontrada aticulo"
+
+
+    
+    articulo = articuloFinal.extend([titulo, subtitulo, etiqueta,localizacion,articuloContenido])
+    print(articulo)
+
+    df = pd.DataFrame({linkArticulo: articuloFinal})
+   
+    df.to_csv(f'articulos_x_procesar/ELPAIS_ESP1/{origen}_{snap}_{count}_ESP.csv', index=False)
+    count += 1
+
+    '''    
+  
     if titulo != "NONE" and subtitulo != "Hrm.":
-        articuloFinal.extend([titulo, subtitulo])
+        articuloFinal.extend([titulo, subtitulo, etiqueta])
         for itemContenido in contenido:
             textoLineas = itemContenido.text.strip().split('\n')
             for linea in textoLineas:
@@ -76,15 +123,16 @@ for articulo, snap in tqdm(urlsArticulos.items(), desc="Procesando artículos"):
                     articuloFinal.append(linea)
 
         df = pd.DataFrame({linkArticulo: articuloFinal})
-        df.to_csv(f'articulos_x_procesar/ELPAIS_ESP/{origen}_{snap}_{count}_ESP.csv', index=False)
+        df.to_csv(f'articulos_x_procesar/ELPAIS_ESP1/{origen}_{snap}_{count}_ESP.csv', index=False)
         count += 1
     else:
         articuloError.append(linkArticulo)
+    '''
 
 driver.quit()
 
 if articuloError:
     df3 = pd.DataFrame({'': articuloError})
-    df3.to_csv('log_ejecuciones/ELPAIS_ESP/articulosError_2020_2025_ESP.csv', index=False)
+    df3.to_csv('log_ejecuciones/ELPAIS_ESP1/articulosError_2020_2025_ESP.csv', index=False)
 
 print("Procesamiento de artículos finalizado.")
